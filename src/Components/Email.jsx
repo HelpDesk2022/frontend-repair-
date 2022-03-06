c0d76508b402eddee4bb7c473234d8f861b17d78
AndyVF <andersonfinho@hotmail.com>
2022-03-05 23:09:35 -0300

    em andamento

--- src/Components/Email.jsx
+++ src/Components/Email.jsx
@@ -1,25 +1,51 @@
 import React from "react";
-import "./email.css"
-import Elipse from "../fotos/Elipse.png"
-import Repair from "../fotos/Repair.png"
+import "./email.css";
+import Elipse from "../fotos/Elipse.png";
+import Repair from "../fotos/Repair.png";
+import {useForm} from "react-hook-form"
 
-export default function () {
+import {Link} from "react-router-dom"
+
+
+   function Email() {
+
+     const {register, formState: {errors}, handleSubmit} = useForm();
+     const onSubmit = data => {
+     
+     };
+      
+  
   return (
     <>
-    <div className="container">
-    <img className="fundo" src={Elipse} alt="Elipse" />
-    </div>
-    <img className="logo" src={Repair} alt="Repair" />
+      <div className="container">
+        <img className="fundo" src={Elipse} alt="Elipse" />
+      </div>
+      <img className="logo" src={Repair} alt="Repair" />
       <div className="login">
+      
         <h1>Faça seu login</h1>
-        <p><b>E-mail</b></p>
-        <form className="box">
-          <input id="Email" className="form_field" type="Email"/> 
-          <button type="button"  className="form_button">Continuar</button>
-          
+        <p>
+          <b>E-mail</b>
+        </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        
+        <input className="form_field"
+           {...register("Email", {required: true})}/>
+           <p className="erro">{errors.Email?.type === 'required' && "Preencha o campo de E-mail"}</p>
+           
+          <br></br>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+
+          <button type="submit"  id="botao" className="form_button">Continuar</button>
         </form>
       </div>
     </>
   );
-  }
+};
 
+export default Email;
--- src/Components/Senha.jsx
+++ src/Components/Senha.jsx
@@ -1,24 +1,79 @@
-import React from "react";
-import "./email.css"
-import Elipse from "../fotos/Elipse.png"
-import Repair from "../fotos/Repair.png"
+import { useState, useEffect } from "react";
+import "./senha.css";
+import Elipse from "../fotos/Elipse.png";
+import Repair from "../fotos/Repair.png";
+import { Link } from "react-router-dom";
 
-export default function () {
+function Senha() {
+  const initialValues = { username: "", email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    if (!values.email) {
+      errors.password = "Por favor, digite sua senha!";
+    } else if (!regex.test(values.password)) {
+      errors.password = "Senha incorreta";
+    }
+
+    return errors;
+  };
   return (
     <>
-    <div className="container1">
-    <img className="fundo" src={Elipse} alt="Elipse" />
-    </div>
-    <img className="logo" src={Repair} alt="Repair" />
+      <div className="container1">
+        <img className="fundo" src={Elipse} alt="Elipse" />
+      </div>
+      <img className="logo" src={Repair} alt="Repair" />
       <div className="Senha">
         <h1>Faça seu login</h1>
-        <p><b>E-mail</b></p>
-        <form className="box">
-          <input id="Senha" className="field" type="Password"/>
-          <button type="button"  className="form_botton">Entrar</button>
-          
+        <p>
+          <b>Senha</b>
+        </p>
+        <form onSubmit={handleSubmit} className="box">
+        <p className="falha">{formErrors.password}</p>
+        <input
+              type="password"
+              id="Senha"
+              className="field"
+              name="password"
+              value={formValues.password}
+              onChange={handleChange}
+          />
+          <br></br>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Link to="/" className="form_botton">
+            Entrar
+          </Link>
         </form>
       </div>
     </>
   );
-  }
+}
+
+export default Senha;
